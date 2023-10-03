17142b8ea3a1ca7d57dde730fc7c028d4f1df738
yves137 <oneg2981@gmail.com>
2023-10-03 16:56:23 +0200

    update project

--- src/CustomButtons.js
+++
@@ -1,8 +0,0 @@
-export function CustomButtons({handleClick}){
-   return <button
-        onClick={() => handleClick()}
-        className="text-white text-2xl font-bold bg-red-600 py-4 px-20 hover:bg-red-700 rounded-md"
-      >
-        Fetch Random
-      </button>
-}
